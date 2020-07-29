08e0eea56665366f7bea091bc87aecc57be94aad
gustavochm <gustavoch@outlook.com>
2020-07-29 15:09:39 -0400

    density solver mixture pep8

--- sgtpy/mixtures/density_solver.py
+++ sgtpy/mixtures/density_solver.py
@@ -2,12 +2,13 @@ import numpy as np
 from scipy.optimize import minimize_scalar, brentq
 from ..constants import kb, Na
 
+
 def dPsaft_fun(rho, x, T, saft):
-    rhomolecular = Na * rho 
+    rhomolecular = Na * rho
     _, dafcn, d2afcn = saft.d2afcn_drho(x, rhomolecular, T)
     dPsaft = 2 * rhomolecular * dafcn + rhomolecular**2 * d2afcn
-    #dPsaft /= Na
     return dPsaft
+
 
 def Psaft_obj(rho, x, T, saft, Pspec):
     rhomolecular = Na * rho
@@ -15,30 +16,28 @@ def Psaft_obj(rho, x, T, saft, Pspec):
     Psaft = rhomolecular**2 * dafcn / Na
     return Psaft - Pspec
 
+
 def density_topliss(state, x, T, P, saft):
-    
-    #lower boundary a zero density
+
+    # lower boundary a zero density
     rho_lb = 1e-5
-    #P_lb = 0.
+
     dP_lb = Na * kb * T
-    
-    #upper boundary limit at infinity pressure
+
+    # Upper boundary limit at infinity pressure
     etamax = 0.7405
-    #rho_lim = np.dot(x, (6 * etamax) / (saft.ms * np.pi * saft.sigma**3)) / Na
-    rho_lim =  (6 * etamax) / np.dot(x, (saft.ms * np.pi * saft.sigma**3)) / Na
-    #Mejorar esta parte con el maximo valor posible de eta de la densidad
+    rho_lim = (6 * etamax) / np.dot(x, (saft.ms * np.pi * saft.sigma**3)) / Na
     ub_sucess = False
-    rho_ub = 0.4 *  rho_lim
+    rho_ub = 0.4 * rho_lim
     it = 0
     while not ub_sucess and it < 5:
         it += 1
         P_ub, dP_ub = saft.dP_drho(x, rho_ub, T)
-        rho_ub += 0.1 *  rho_lim
+        rho_ub += 0.1 * rho_lim
         ub_sucess = P_ub > P and dP_ub > 0
-    
-    #Calculo de derivada numerica en densidad nula
+
+    # Derivative calculation at zero density
     rho_lb1 = 1e-4 * rho_lim
-    #rho_lb1 = 1e-5
     P_lb1, dP_lb1 = saft.dP_drho(x, rho_lb1, T)
     d2P_lb1 = (dP_lb1 - dP_lb) / rho_lb1
     if d2P_lb1 > 0:
@@ -46,12 +45,12 @@ def density_topliss(state, x, T, P, saft):
     else:
         flag = 1
 
-    #Comienza Etapa 1
+    # Stage 1
     bracket = [rho_lb, rho_ub]
     if flag == 1:
-        #Se debe encontrar el punto de inflexion
-        sol_inf = minimize_scalar(dPsaft_fun, args = (x, T, saft),
-                                  bounds = bracket, method = 'Bounded' )
+        # Found inflexion point
+        sol_inf = minimize_scalar(dPsaft_fun, args=(x, T, saft),
+                                  bounds=bracket, method='Bounded')
         rho_inf = sol_inf.x
         dP_inf = sol_inf.fun
         if dP_inf > 0:
@@ -59,13 +58,13 @@ def density_topliss(state, x, T, P, saft):
         else:
             flag = 2
 
-    #Etapa 2
+    # Stage 2
     if flag == 2:
         if state == 'L':
             bracket[0] = rho_inf
         elif state == 'V':
             bracket[1] = rho_inf
-        rho_ext = brentq(dPsaft_fun, bracket[0], bracket[1], args =(x, T, saft))
+        rho_ext = brentq(dPsaft_fun, bracket[0], bracket[1], args=(x, T, saft))
         P_ext, dP_ext = saft.dP_drho(x, rho_ext, T)
         if P_ext > P and state == 'V':
             bracket[1] = rho_ext
@@ -77,13 +76,13 @@ def density_topliss(state, x, T, P, saft):
     if flag == -1:
         rho = np.nan
     else:
-        rho = brentq(Psaft_obj, bracket[0], bracket[1], args = (x, T, saft, P))
+        rho = brentq(Psaft_obj, bracket[0], bracket[1], args=(x, T, saft, P))
 
     return rho
 
 
 def density_newton(rho0, x, T, P, saft):
-    
+
     rho = 1.*rho0
     Psaft, dPsaft = saft.dP_drho(x, rho, T)
     FO = Psaft - P
