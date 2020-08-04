4b065d8914b18dae57efd239d21a4e508ca79f7b
gustavochm <gustavoch@outlook.com>
2020-08-04 10:55:23 -0400

    updated to be faster for associating mixtures

--- sgtpy/mixtures/density_solver.py
+++ sgtpy/mixtures/density_solver.py
@@ -1,28 +1,32 @@
 import numpy as np
 from scipy.optimize import minimize_scalar, brentq
-from ..constants import kb, Na
+from ..constants import Na
 
 
-def dPsaft_fun(rho, x, T, saft):
+def dPsaft_fun(rho, x, temp_aux, saft):
     rhomolecular = Na * rho
-    _, dafcn, d2afcn = saft.d2afcn_drho(x, rhomolecular, T)
+    global Xass
+    da, Xass = saft.d2afcn_drho_aux(x, rhomolecular, temp_aux, Xass)
+    afcn, dafcn, d2afcn = da
     dPsaft = 2 * rhomolecular * dafcn + rhomolecular**2 * d2afcn
     return dPsaft
 
 
-def Psaft_obj(rho, x, T, saft, Pspec):
+def Psaft_obj(rho, x, temp_aux, saft, Pspec):
     rhomolecular = Na * rho
-    _, dafcn = saft.dafcn_drho(x, rhomolecular, T)
+    global Xass
+    da, Xass = saft.dafcn_drho_aux(x, rhomolecular, temp_aux, Xass)
+    afcn, dafcn = da
     Psaft = rhomolecular**2 * dafcn / Na
     return Psaft - Pspec
 
 
-def density_topliss(state, x, T, P, saft):
+def density_topliss(state, x, temp_aux, P, Xass0, saft):
 
+    beta = temp_aux[0]
     # lower boundary a zero density
     rho_lb = 1e-5
-
-    dP_lb = Na * kb * T
+    dP_lb = Na / beta
 
     # Upper boundary limit at infinity pressure
     etamax = 0.7405
@@ -30,26 +34,30 @@ def density_topliss(state, x, T, P, saft):
     ub_sucess = False
     rho_ub = 0.4 * rho_lim
     it = 0
+    P_ub, dP_ub, Xass_ub = saft.dP_drho_aux(x, rho_ub, temp_aux, Xass0)
     while not ub_sucess and it < 5:
         it += 1
-        P_ub, dP_ub = saft.dP_drho(x, rho_ub, T)
+        P_ub, dP_ub, Xass_ub = saft.dP_drho_aux(x, rho_ub, temp_aux, Xass_ub)
         rho_ub += 0.1 * rho_lim
         ub_sucess = P_ub > P and dP_ub > 0
 
     # Derivative calculation at zero density
     rho_lb1 = 1e-4 * rho_lim
-    P_lb1, dP_lb1 = saft.dP_drho(x, rho_lb1, T)
+    P_lb1, dP_lb1, Xass_lb = saft.dP_drho_aux(x, rho_lb1, temp_aux, Xass0)
     d2P_lb1 = (dP_lb1 - dP_lb) / rho_lb1
     if d2P_lb1 > 0:
         flag = 3
     else:
         flag = 1
 
+    global Xass
+    Xass = Xass0
+
     # Stage 1
     bracket = [rho_lb, rho_ub]
     if flag == 1:
         # Found inflexion point
-        sol_inf = minimize_scalar(dPsaft_fun, args=(x, T, saft),
+        sol_inf = minimize_scalar(dPsaft_fun, args=(x, temp_aux, saft),
                                   bounds=bracket, method='Bounded')
         rho_inf = sol_inf.x
         dP_inf = sol_inf.fun
@@ -64,8 +72,9 @@ def density_topliss(state, x, T, P, saft):
             bracket[0] = rho_inf
         elif state == 'V':
             bracket[1] = rho_inf
-        rho_ext = brentq(dPsaft_fun, bracket[0], bracket[1], args=(x, T, saft))
-        P_ext, dP_ext = saft.dP_drho(x, rho_ext, T)
+        rho_ext = brentq(dPsaft_fun, bracket[0], bracket[1],
+                         args=(x, temp_aux, saft))
+        P_ext, dP_ext, Xass = saft.dP_drho_aux(x, rho_ext, temp_aux, Xass)
         if P_ext > P and state == 'V':
             bracket[1] = rho_ext
         elif P_ext < P and state == 'L':
@@ -76,22 +85,22 @@ def density_topliss(state, x, T, P, saft):
     if flag == -1:
         rho = np.nan
     else:
-        rho = brentq(Psaft_obj, bracket[0], bracket[1], args=(x, T, saft, P))
+        rho = brentq(Psaft_obj, bracket[0], bracket[1],
+                     args=(x, temp_aux, saft, P))
 
-    return rho
+    return rho, Xass
 
 
-def density_newton(rho0, x, T, P, saft):
+def density_newton(rho0, x, temp_aux, P, Xass0, saft):
 
     rho = 1.*rho0
-    Psaft, dPsaft = saft.dP_drho(x, rho, T)
-    FO = Psaft - P
-    dFO = dPsaft
-    for i in range(30):
-        rho -= FO/dFO
-        Psaft, dPsaft = saft.dP_drho(x, rho, T)
+    Psaft, dPsaft, Xass = saft.dP_drho_aux(x, rho, temp_aux, Xass0)
+    for i in range(15):
         FO = Psaft - P
         dFO = dPsaft
-        if np.abs(FO) < 1e-10:
+        drho = FO/dFO
+        rho -= drho
+        if np.abs(drho) < 1e-8:
             break
-    return rho
+        Psaft, dPsaft, Xass = saft.dP_drho_aux(x, rho, temp_aux, Xass)
+    return rho, Xass
