e358cb2157b460507638155abe3477ee678099b6
gustavochm <gustavoch@outlook.com>
2021-12-10 15:30:23 +0000

    updated setup.py to force cython compilation

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ except ImportError:
 else:
     use_cython = True
 
+"""
 cmdclass = {}
 ext_modules = []
+
 
 if use_cython:
     ext_modules += [Extension('sgtpy.coloc_cy',
@@ -21,8 +23,8 @@ else:
     ext_modules += [Extension('sgtpy.coloc_cy', ['sgtpy/src/coloc_cy.c']),
                     Extension('sgtpy.sgt.cijmix_cy',
                     ['sgtpy/src/cijmix_cy.c'])]
-
 """
+
 cmdclass = {}
 ext_modules = []
 
@@ -32,7 +34,7 @@ ext_modules += [Extension('SGTPy.coloc_cy',
                 Extension('SGTPy.sgt.cijmix_cy',
                           ['SGTPy/src/cijmix_cy.pyx'])]
 cmdclass.update({'build_ext': build_ext})
-"""
+
 
 setup(
   name='sgtpy',
